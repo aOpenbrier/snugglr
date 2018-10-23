f6ddcad76d3a5a81c44b3aa71c335bd256f4f3d2
aOpenbrier <aopenbrier@gmail.com>
2018-10-23 00:02:27 -0700

    match data

---
+++ app/data/matches.js
@@ -0,0 +1,28 @@
+let matches = [
+    {
+        name: "Dog",
+        photo: "images/dog.jpg",
+        scores: [3, 5, 2, 4, 4, 3, 3, 3, 3, 5]
+    },
+    {
+        name: "Cat",
+        photo: "images/cat.jpg",
+        scores: [5, 2, 4, 3, 1, 1, 2, 5, 1, 2]
+    },
+    {
+        name: "Raccoon",
+        photo: "images/raccoon.jpg",
+        scores: [4, 1, 5, 5, 2, 1, 1, 2, 1, 3]
+    },
+    {
+        name: "Dolphin",
+        photo: "images/dolphin.jpg",
+        scores: [5, 4, 3, 1, 2, 5, 2, 4, 5, 4]
+    },
+    {
+        name: "Piglet",
+        photo: "images/piglet.jpg",
+        scores: [2, 3, 2, 4, 5, 3, 5, 2, 2, 4]
+    }
+]
+
