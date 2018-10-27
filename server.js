9c29b9fb80b46867f3d4c38e2924602b0950fcbc
aOpenbrier <aopenbrier@gmail.com>
2018-10-27 00:18:25 -0700

    body-parser of course

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 3000
+const bodyParser = require('body-parser')
 
 app.use(express.static(path.join(__dirname, 'app/public'))) 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json()) 				     
 
 require('./app/routing/apiRoutes')(app)
 require('./app/routing/htmlRoutes')(app)
